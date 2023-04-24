7df0088b55470ae8b2443fb663b190f207522d74
Arthur Silva <arthurmatsilva@outlook.com>
2023-04-24 17:18:11 -0300

    feat: drag and drop

--- components/CityDetails/index.tsx
+++ components/CityDetails/index.tsx
@@ -1,50 +1,104 @@
 import React from "react";
+import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
 
 import { getDistance, getTime, getWindSpeed } from "@/utils/helpers";
 import { InfoCard } from "@/components";
 import { degToCompass } from "@/utils/format";
 import { CityContext } from "@/contexts/city";
 
+interface ItemProps {
+  title: string;
+  value: string | number;
+  sub?: string;
+  icon: string;
+}
+
 export function CityDetails() {
+  const [items, setItems] = React.useState<ItemProps[]>([]);
   const { city, system } = React.useContext(CityContext);
 
+  const onDragEndHandler = (result: DropResult) => {
+    const { destination, source } = result;
+
+    if (
+      !destination ||
+      (destination.droppableId === source.droppableId &&
+        destination.index === source.index)
+    )
+      return;
+
+    const itemList = [...items];
+    const aux = {...itemList[destination.index]};
+    itemList[destination.index] = {...itemList[source.index]};
+    itemList[source.index] = {...aux};
+    setItems(itemList);
+  };
+
+  React.useEffect(() => {
+    const itemList = [];
+    itemList.push({
+      title: "Humidity",
+      value: city.humidity,
+      sub: "%",
+      icon: "/droplet.svg",
+    });
+    itemList.push({
+      title: "Wind speed",
+      value: getWindSpeed(system, city.wind_speed),
+      sub: system === "metric" ? "m/s" : "m/h",
+      icon: "/wind.svg",
+    });
+    itemList.push({
+      title: "Wind direction",
+      value: degToCompass(city?.wind_direction),
+      icon: "/compass.svg",
+    });
+    itemList.push({
+      title: "Visibility",
+      value: getDistance(system, city?.visibility),
+      sub: system === "metric" ? "km" : "miles",
+      icon: "/eye.svg",
+    });
+    itemList.push({
+      title: "Sunrise",
+      value: getTime(system, city?.sunrise, city?.timezone) || "",
+      sub: system === "metric" ? "" : "AM",
+      icon: "/sunrise.svg",
+    });
+    itemList.push({
+      title: "Sunset",
+      value: getTime(system, city?.sunset, city?.timezone) || "",
+      sub: system === "metric" ? "" : "PM",
+      icon: "/sunset.svg",
+    });
+    setItems(itemList);
+  }, [system, city]);
+
   return (
-    <div className="w-full flex flex-wrap justify-center gap-2 sm:gap-4 lg:w-3/4">
-      <InfoCard
-        title="Humidity"
-        value={city.humidity}
-        sub="%"
-        icon="/droplet.svg"
-      />
-      <InfoCard
-        title="Wind speed"
-        value={getWindSpeed(system, city.wind_speed)}
-        sub={system === "metric" ? "m/s" : "m/h"}
-        icon="/wind.svg"
-      />
-      <InfoCard
-        title="Wind direction"
-        value={degToCompass(city?.wind_direction)}
-        icon="/compass.svg"
-      />
-      <InfoCard
-        title="Visibility"
-        value={getDistance(system, city?.visibility)}
-        sub={system === "metric" ? "km" : "miles"}
-        icon="/eye.svg"
-      />
-      <InfoCard
-        title="Sunrise"
-        value={getTime(system, city?.sunrise, city?.timezone) || ""}
-        sub={system === "metric" ? "" : "AM"}
-        icon="/sunrise.svg"
-      />
-      <InfoCard
-        title="Sunset"
-        value={getTime(system, city?.sunset, city?.timezone) || ""}
-        sub={system === "metric" ? "" : "PM"}
-        icon="/sunset.svg"
-      />
-    </div>
+    <DragDropContext onDragEnd={onDragEndHandler}>
+      <Droppable droppableId="details">
+        {(droppableProvided, droppableSnapshot) => (
+          <div
+            className={`w-full flex flex-wrap justify-center gap-2 sm:gap-4 lg:w-3/4 ${
+              droppableSnapshot.isDraggingOver ? "opacity-80" : ""
+            }`}
+            ref={droppableProvided.innerRef}
+            {...droppableProvided.droppableProps}
+          >
+            {items.map((item, index) => 
+              <InfoCard
+                key={index}
+                index={index}
+                title={item.title}
+                value={item.value}
+                sub={item.sub}
+                icon={item.icon}
+              />  
+            )}
+            {droppableProvided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 }
--- components/InfoCard/index.tsx
+++ components/InfoCard/index.tsx
@@ -1,25 +1,36 @@
+import { Draggable } from "react-beautiful-dnd";
+
 interface InfoCardProps {
+  index: number;
   title: string;
   value: string | number;
   sub?: string;
   icon: string;
 }
 
-export function InfoCard({ title, value, sub, icon }: InfoCardProps) {
+export function InfoCard({ title, index, value, sub, icon }: InfoCardProps) {
   return (
-    <section className="flex justify-between items-center bg-slate-100 rounded-xl p-2 shadow w-32 h-20 sm:w-40 sm:h-24 md:w-44 md:h-32 xl:w-56 xl:p-2">
-      <img className="w-6 sm:w-10 md:w-14 xl:w-20" src={icon} alt="icon" />
-      <div className="flex flex-col items-end">
-        <p className="text-dark text-right text-sm sm:text-base md:text-lg xl:text-2xl">
-          {title}
-        </p>
-        <p className="font-semibold font-mono text-base sm:text-xl md:text-2xl xl:text-4xl">
-          {value}
-        </p>
-        <p className="text-dark text-sm sm:text-base md:text-lg xl:text-2xl">
-          {sub}
-        </p>
-      </div>
-    </section>
+    <Draggable draggableId={index.toString()} index={index}>
+      {(draggableProvided) => (
+        <section className="flex justify-between items-center bg-slate-100 rounded-xl p-2 shadow w-32 h-20 sm:w-40 sm:h-24 md:w-44 md:h-32 xl:w-56 xl:p-2"
+          {...draggableProvided.draggableProps}
+          {...draggableProvided.dragHandleProps}
+          ref={draggableProvided.innerRef}
+        >
+          <img className="w-6 sm:w-10 md:w-14 xl:w-20" src={icon} alt="icon" />
+          <div className="flex flex-col items-end">
+            <p className="text-dark text-right text-sm sm:text-base md:text-lg xl:text-2xl">
+              {title}
+            </p>
+            <p className="font-semibold font-mono text-base sm:text-xl md:text-2xl xl:text-4xl">
+              {value}
+            </p>
+            <p className="text-dark text-sm sm:text-base md:text-lg xl:text-2xl">
+              {sub}
+            </p>
+          </div>
+        </section>
+      )}
+    </Draggable>
   );
 }
